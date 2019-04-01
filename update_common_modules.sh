5d6e7ba0bb34283c52fdddce901f7569a81070a6
Matt Woodward <matthew.woodward@swift-nav.com>
2019-04-01 12:40:53 +1100

    Start Find* modules

--- update_common_modules.sh
+++ update_common_modules.sh
@@ -20,33 +20,38 @@
 #
 ############################################################################
 
-if ! git remote >/dev/null 2>&1;
+if [ "x$1" == "SKIP_CHECKS" ];
 then
-  echo "This doesn't look like a git repository. You probably don't want"
-  echo "to run this script in this location"
-  exit 1
-fi
-
-if git rev-list a34c3c8814b0d75ddb56162b6d282362ed6b9a9e >/dev/null 2>&1 ;
-then
-  echo "This script must not be run from the Swift common cmake modules repository"
-  echo "Change to the local cmake directory in the project you wish to update"
-  echo "and run it again from there"
-  exit 1
-fi
-
-if ! git diff-index --cached --quiet HEAD ;
-then
-  echo "You have staged changes. Commit or unstage them before"
-  echo "running this command"
-  exit 1
-fi
-
-if ! git diff-index --quiet HEAD ;
-then
-  echo "You have uncommitted changes. Commit or stash them"
-  echo "before running this command"
-  exit 1
+  shift;
+else
+  if ! git remote >/dev/null 2>&1;
+  then
+    echo "This doesn't look like a git repository. You probably don't want"
+    echo "to run this script in this location"
+    exit 1
+  fi
+  
+  if git rev-list a34c3c8814b0d75ddb56162b6d282362ed6b9a9e >/dev/null 2>&1 ;
+  then
+    echo "This script must not be run from the Swift common cmake modules repository"
+    echo "Change to the local cmake directory in the project you wish to update"
+    echo "and run it again from there"
+    exit 1
+  fi
+  
+  if ! git diff-index --cached --quiet HEAD ;
+  then
+    echo "You have staged changes. Commit or unstage them before"
+    echo "running this command"
+    exit 1
+  fi
+  
+  if ! git diff-index --quiet HEAD ;
+  then
+    echo "You have uncommitted changes. Commit or stash them"
+    echo "before running this command"
+    exit 1
+  fi
 fi
 
 BRANCH=master
