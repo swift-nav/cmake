0539d09bc9c338791ff8f12c1d4d7e3b1671bcc6
Matt Woodward <matthew.woodward@swift-nav.com>
2019-04-03 18:59:31 +1100

    Update gnss-converters

--- Findgnss_converters.cmake
+++ Findgnss_converters.cmake
@@ -1,4 +1,4 @@
-if(TARGET gnss-converters)
+if(TARGET gnss_converters)
   return()
 endif()
 
@@ -10,26 +10,26 @@ endif()
 if(EXISTS "${SUBMOD_PATH}/CMakeLists.txt")
   message(STATUS "Found gnss-converters source in ${SUBMOD_PATH}")
   add_subdirectory("${SUBMOD_PATH}")
-  if(NOT TARGET gnss-converters)
+  if(NOT TARGET gnss_converters)
     message(FATAL_ERROR "Including directory ${SUBMOD_PATH} did not generate target gnss-converters as expected")
   endif()
   return()
 endif()
 
-find_path(GNSS_CONVERTERS_INCLUDE_DIR libgnss-converters/gnss-converters.h)
+find_path(GNSS_CONVERTERS_INCLUDE_DIR libgnss-converters/nmea.h)
 
-set(GNSS_CONVERTERS_LIB_NAMES ${GNSS_CONVERTERS_LIB_NAMES} gnss-converters libgnss-converters)
+set(GNSS_CONVERTERS_LIB_NAMES ${GNSS_CONVERTERS_LIB_NAMES} gnss_converters libgnss_converters)
 find_library(GNSS_CONVERTERS_LIBRARY NAMES ${GNSS_CONVERTERS_LIB_NAMES})
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(gnss-converters DEFAULT_MSG GNSS_CONVERTERS_LIBRARY GNSS_CONVERTERS_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(gnss_converters DEFAULT_MSG GNSS_CONVERTERS_LIBRARY GNSS_CONVERTERS_INCLUDE_DIR)
 
 if(GNSS_CONVERTERS_FOUND)
-  add_library(gnss-converters UNKNOWN IMPORTED)
-  set_target_properties(gnss-converters PROPERTIES
+  add_library(gnss_converters UNKNOWN IMPORTED)
+  set_target_properties(gnss_converters PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${SBP_INCLUDE_DIRS}")
 
-  set_property(TARGET gnss-converters APPEND PROPERTY
+  set_property(TARGET gnss_converters APPEND PROPERTY
       IMPORTED_LOCATION "${GNSS_CONVERTERS_LIBRARY}")
 endif()
 
