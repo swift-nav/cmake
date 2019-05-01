95bf587a8c41d84290f237ad4cd770eb59aa43de
Matt Woodward <matthew.woodward@swift-nav.com>
2019-05-01 16:08:48 +1000

    Mark gflags as system, add gnss-converters

--- FindGFlags.cmake
+++ FindGFlags.cmake
@@ -3,3 +3,13 @@ GenericFindDependency(
     TargetName gflags
     SourceDir "googleflags"
     )
+
+if(NOT CMAKE_CROSSCOMPILING OR THIRD_PARTY_INCLUDES_AS_SYSTEM)
+  # Change all of GoogleFlags's include directories to be system includes, to avoid
+  # compiler errors
+  get_target_property(gflags_include_directories gflags_nothreads_static INTERFACE_INCLUDE_DIRECTORIES)
+  if(gflags_include_directories)
+    set_target_properties(gflags_nothreads_static PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
+    target_include_directories(gflags_nothreads_static SYSTEM INTERFACE ${gflags_include_directories})
+  endif()
+endif()
---
+++ FindGnss-converters.cmake
@@ -0,0 +1,5 @@
+include("GenericFindDependency")
+GenericFindDependency(
+    TargetName gnss_converters
+    SourceDir "gnss-converters/c"
+    )
