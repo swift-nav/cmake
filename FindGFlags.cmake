6a88f07e95cde88bc314721e7979e9ff69ff5abf
Matt Woodward <matthew.woodward@swift-nav.com>
2019-05-01 18:38:05 +1000

    Alter gflags alias target

--- FindGFlags.cmake
+++ FindGFlags.cmake
@@ -7,9 +7,12 @@ GenericFindDependency(
 if(NOT CMAKE_CROSSCOMPILING OR THIRD_PARTY_INCLUDES_AS_SYSTEM)
   # Change all of GoogleFlags's include directories to be system includes, to avoid
   # compiler errors
-  get_target_property(gflags_include_directories gflags_nothreads_static INTERFACE_INCLUDE_DIRECTORIES)
-  if(gflags_include_directories)
-    set_target_properties(gflags_nothreads_static PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
-    target_include_directories(gflags_nothreads_static SYSTEM INTERFACE ${gflags_include_directories})
+  get_target_property(_aliased gflags ALIASED_TARGET)
+  if(_aliased)
+    get_target_property(gflags_include_directories ${_aliased} INTERFACE_INCLUDE_DIRECTORIES)
+    if(gflags_include_directories)
+      set_target_properties(${_aliased} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
+      target_include_directories(${_aliased} SYSTEM INTERFACE ${gflags_include_directories})
+    endif()
   endif()
 endif()
