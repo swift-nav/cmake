1fcb326dde3fc99e750e7f2e08822839923f3d36
Matt Woodward <matthew.woodward@swift-nav.com>
2019-05-09 16:15:55 +1000

    Recode Optional's header files as system

--- FindOptional.cmake
+++ FindOptional.cmake
@@ -2,3 +2,14 @@ include("GenericFindDependency")
 GenericFindDependency(
     TargetName optional
     )
+
+if(NOT CMAKE_CROSSCOMPILING OR THIRD_PARTY_INCLUDES_AS_SYSTEM)
+  # Change all of Optional's include directories to be system includes, to avoid
+  # compiler errors
+  get_target_property(optional_include_directories optional INTERFACE_INCLUDE_DIRECTORIES)
+  if(optional_include_directories)
+    message(WARNING "Resetting optional includes ${optional_include_directories}")
+    set_target_properties(optional PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
+    target_include_directories(optional SYSTEM INTERFACE ${optional_include_directories})
+  endif()
+endif()
