685120035f252b14ec5557aedf23a118f317486d
Matt Woodward <matthew.woodward@swift-nav.com>
2019-05-14 16:07:52 +1000

    Add RapidCheck handler

--- FindOptional.cmake
+++ FindOptional.cmake
@@ -8,7 +8,6 @@ if(NOT CMAKE_CROSSCOMPILING OR THIRD_PARTY_INCLUDES_AS_SYSTEM)
   # compiler errors
   get_target_property(optional_include_directories optional INTERFACE_INCLUDE_DIRECTORIES)
   if(optional_include_directories)
-    message(WARNING "Resetting optional includes ${optional_include_directories}")
     set_target_properties(optional PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "")
     target_include_directories(optional SYSTEM INTERFACE ${optional_include_directories})
   endif()
