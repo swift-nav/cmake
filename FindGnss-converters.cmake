b591a9f4e2414cef10f7371fa48fad68069982f0
Matt Woodward <matthew.woodward@swift-nav.com>
2019-06-17 13:31:26 +1000

    Specify header file name for non-standard installs

--- FindGnss-converters.cmake
+++ FindGnss-converters.cmake
@@ -2,5 +2,6 @@ include("GenericFindDependency")
 GenericFindDependency(
     TARGET gnss_converters
     SOURCE_DIR "gnss-converters/c"
+    SYSTEM_HEADER_FILE "gnss-converters/nmea.h"
     SYSTEM_INCLUDES
     )
--- FindRtcm.cmake
+++ FindRtcm.cmake
@@ -2,5 +2,6 @@ include("GenericFindDependency")
 GenericFindDependency(
     TARGET rtcm
     SOURCE_DIR "c"
+    SYSTEM_HEADER_FILE "rtcm3/bits.h"
     SYSTEM_INCLUDES
     )
