8338f3d34b5e611e8fa67eb0856a91c95dafbcf1
Matt Woodward <46688854+woodfell@users.noreply.github.com>
2019-09-13 06:44:27 +1000

    Set default options for common dependencies (#24)

--- FindPal.cmake
+++ FindPal.cmake
@@ -1,4 +1,6 @@
 include("GenericFindDependency")
+option(pal_ENABLE_TESTS "" OFF)
+option(pal_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
   TARGET pal
   SYSTEM_INCLUDES
--- FindStarling.cmake
+++ FindStarling.cmake
@@ -1,4 +1,6 @@
 include("GenericFindDependency")
+option(starling_ENABLE_TESTS "" OFF)
+option(starling_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
     TARGET starling
     SYSTEM_HEADER_FILE "starling/starling.h"
