630319bff0adf946eb15c36666ab3e843cc093de
Matt Woodward <matthew.woodward@swift-nav.com>
2019-10-22 10:27:40 +1100

    Add comments

--- TestTargets.cmake
+++ TestTargets.cmake
@@ -1,3 +1,108 @@
+# Helper functions for creating cmake targets to build and run test suites
+#
+# This module defines several cmake targets which support building and running 
+# tests. 2 external function are available with subtle differences, 
+# swift_add_test() and swift_add_test_runner()
+#
+# swift_add_test() defines a new test suite to be built from a list of sources 
+# with options for linking libraries and including extra source directories. 
+# Basic usage is
+#
+# swift_add_test(test-suite-name
+#    SRCS
+#      main.cc
+#      ...more source files
+#    LINK
+#      ...link libraries
+#    INCLUDE
+#      ...extra include directories
+#    )
+#
+# This will create 2 cmake targets
+# - test-suite-name - an executable composed of the specified source files, 
+#   linked against the specified libraries
+# - do-test-suite-name - A custom target which will execute the above target
+#
+#
+# LINK and INCLUDE are optional parameters. 
+#
+# All tests will have their language standards set to the swift default by
+# using the LanguageStandards module, and have code coverage enabled
+#
+# An optional parameter PARALLEL can be passed which will create an additional 
+# target to run tests in parallel.
+#
+# swift_add_test(test-suite-name
+#   PARALLEL
+#   SRCS ...srcs...
+#   )
+#
+# will create an extra target parallel-test-suite-name. This target only makes 
+# sense for gtest based test suites and uses the parallel test helper program 
+# from the googletest package. The non-parallel target is always created.
+#
+# The other function, swift_add_test_runner(), can be used to create a do-... 
+# target which pointer at some other command. This can be used to invoke a test 
+# which is actually a shell script or some other executable which is available 
+# but not built from source
+#
+# swift_add_test_runner(test-suite-name
+#    COMMAND <shell-command-to-run-test>
+#    DEPENDS <optional list of cmake targets on which this test depend>
+#    )
+#
+# This will create just a single target:
+# - do-test-suite-name - Execute the given command
+#
+# Both functions can take an optional argument COMMENT which will print out a 
+# nicer status message when the test is run
+#
+# swift_add_test(test-suite-name
+#    SRCS <list of source>
+#    COMMENT "Performing some tests"
+#    )
+#
+# This module will create some extra global targets to build and run all tests
+# - build-all-tests - Build all specified tests
+# - do-all-tests - Execute all specified tests
+#
+# In addition tests can be added with the option POST_BUILD which will cause 
+# cmake to execute those tests as part of the 'all' target. To assist this 
+# functionality this module will create some extra targets
+#
+# - build-post-build-tests - Build all tests which have been marked as POST_BUILD
+# - do-post-build-tests - Run all tests which have been marked as POST_BUILD
+#
+# To specify a test to be run as part of 'make all' simply pass the option POST_BUILD
+#
+# swift_add_test(test-suite-name
+#    POST_BUILD
+#    SRCS ...srcs
+#    LINK ...libraries
+#    )
+#
+# or
+#
+# swift_add_test_runner(test-suite-name
+#    POST_BUILD
+#    COMMAND <path to executable>
+#    )
+#
+# Dependency chains are set up so that post build tests will be run towards the 
+# end of the build process. Cmake lacks functionality to run commands as a 
+# post-build step so it is not guaranteed that tests will run after everything 
+# has been built, just that they will run very late in the process. This should 
+# give a chance for compiler errors to surface before tests are run
+#
+# A command line option AUTORUN_TESTS can be specified (ON by default) to 
+# control whether or not tests run as part of 'make all'.
+#
+# cmake -DAUTORUN_TESTS=OFF <path to source>
+#
+# will disable post build tests from running as part of 'make all'. The post-build 
+# targets will still be created and can be invoked manually.
+#
+
 include(LanguageStandards)
 include(CodeCoverage)
 
