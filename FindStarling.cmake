022db10af6958ffdf83c920a038f41a8f611b2f1
Benjamin Altieri <benaltieri@gmail.com>
2019-11-04 14:37:37 -0800

    Disable compiled OTL in starling by default

--- FindStarling.cmake
+++ FindStarling.cmake
@@ -1,6 +1,7 @@
 include("GenericFindDependency")
 option(starling_ENABLE_TESTS "" OFF)
 option(starling_ENABLE_EXAMPLES "" OFF)
+option(starling_ENABLE_COMPILED_OTL "" OFF)
 GenericFindDependency(
     TARGET starling
     SYSTEM_HEADER_FILE "starling/starling.h"
