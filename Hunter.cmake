4753dd8ab9db8526de02421e4ac7389e28f1ac59
Matt Woodward <matthew.woodward@swift-nav.com>
2019-11-06 12:25:22 +1100

    Start hunter helper modules

---
+++ Hunter.cmake
@@ -0,0 +1,7 @@
+include(HunterGate)
+HunterGate(
+  URL "https://github.com/woodfell/hunter/archive/v0.23.214.tar.gz"
+  SHA1 "e14bc153a7f16d6a5eeec845fb0283c8fad8c358"
+  #FILEPATH "${CMAKE_CURRENT_LIST_DIR}/HunterConfig.cmake"
+  )
+
