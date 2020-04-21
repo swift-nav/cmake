8571fcb86e3c6cb38441b4b68440df1bc6c0f0a5
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-04-21 09:47:56 -0700

    Added a SOURCE_DIR option

--- FindStarling.cmake
+++ FindStarling.cmake
@@ -3,6 +3,7 @@ option(starling_ENABLE_TESTS "" OFF)
 option(starling_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
     TARGET pvt_driver
+    SOURCE_DIR starling
     SYSTEM_HEADER_FILE "pvt_driver/pvt_driver.h"
     SYSTEM_INCLUDES
     )
