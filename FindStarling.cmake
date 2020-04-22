9c955d94fb3341bf70c364a0518833017d5b9686
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-04-21 21:15:54 -0700

    Switched to finding the pvt-runner-lib target

--- FindStarling.cmake
+++ FindStarling.cmake
@@ -2,8 +2,8 @@ include("GenericFindDependency")
 option(starling_ENABLE_TESTS "" OFF)
 option(starling_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
-    TARGET pvt_driver
+    TARGET pvt-runner-lib
     SOURCE_DIR starling
-    SYSTEM_HEADER_FILE "pvt_driver/pvt_driver.h"
+    SYSTEM_HEADER_FILE "pvt_driver/runner/pvt_runner.h"
     SYSTEM_INCLUDES
-    )
+)
