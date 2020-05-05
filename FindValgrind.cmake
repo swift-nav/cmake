a8ffefe924a0de51463ebb8850656e0d5acf836c
David <david.enberg@swift-nav.com>
2020-05-05 21:09:26 +0200

    more stuff

--- FindValgrind.cmake
+++ FindValgrind.cmake
@@ -1,6 +1,13 @@
-include("GenericFindDependency")
-option(VALGRIND_ENABLE "" OFF)
-GenericFindDependency(
-    TARGET valgrind
-    SYSTEM_INCLUDES
-    )
+if (NOT VALGRIND_FOUND)
+
+  find_program(VALGRIND_EXECUTABLE valgrind)
+  set(VALGRIND_EXECUTABLE ${VALGRIND_EXECUTABLE} CACHE STRING "")
+
+# handle the QUIETLY and REQUIRED arguments and set VALGRIND_FOUND to TRUE if
+# all listed variables are TRUE
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(Valgrind DEFAULT_MSG VALGRIND_EXECUTABLE)
+
+  mark_as_advanced(VALGRIND_FOUND VALGRIND_EXECUTABLE)
+
+endif (NOT VALGRIND_FOUND)
--- ValgrindUtil.cmake
+++ ValgrindUtil.cmake
@@ -1,11 +1,9 @@
+find_package(Valgrind REQUIRED)
 
+if(NOT VALGRIND_FOUND)
+  message(WARNING "Unable to generate valgrind checks for target \"${target}\" due to missing valgrind package")
+  return()
+endif()
 
-function(swift_add_valgrind target)
-  find_package(valgrind REQUIRED)
-
-  if(NOT VALGRIND_FOUND)
-    message(WARNING "Unable to generate valgrind checks for target \"${target}\" due to missing valgrind package")
-    return()
-  endif()
-
-endfunction()
+add_custom_target(valgrind_all
+  COMMAND valgrind --tool=memcheck)
