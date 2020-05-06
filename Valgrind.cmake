ec85149b306197a8b8da9736c8e10091fc128253
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2020-05-06 16:06:41 +1000

    callgrind introduction

--- Valgrind.cmake
+++ Valgrind.cmake
@@ -88,3 +88,44 @@ function(swift_add_valgrind_memcheck target)
   )
   add_dependencies(do-all-valgrind-tests ${target}-memcheck)
 endfunction()
+
+function(swift_add_valgrind_callgrind target)
+  set(argOption TRACE_CHILDREN)
+  set(argSingle NAME WORKING_DIRECTORY)
+  set(argMulti PROGRAM_ARGS)
+
+  cmake_parse_arguments(x "${argOption}" "${argSingle}" "${argMulti}" ${ARGN})
+
+  if (x_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "Unparsed arguments ${x_UNPARSED_ARGUMENTS}")
+  endif()
+
+  set(target_name ${target}-callgrind)
+  if (x_NAME)
+    set(target_name ${x_NAME})
+  endif()
+
+  set(working_directory ${CMAKE_CURRENT_BINARY_DIR}/${target_name})
+  if (x_WORKING_DIRECTORY)
+    set(working_directory ${working_directory})
+  endif()
+
+  unset(callgrind_options)
+  if (x_TRACE_CHILDREN)
+    list(APPEND callgrind_options --trace-children=yes)
+    list(APPEND callgrind_options --log-file=callgrind.log.%p)
+  else()
+    list(APPEND callgrind_options --log-file=callgrind.log)
+  endif()
+
+  _valgrind_setup(${target})
+
+  add_custom_target(${target_name}
+    COMMENT "Valgrind Callgrind is being applied to \"${target}\""
+    COMMAND ${CMAKE_COMMAND} -E remove_directory ${working_directory}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${working_directory}
+    COMMAND ${CMAKE_COMMAND} -E chdir ${working_directory} ${VALGRIND_EXECUTABLE} --tool=callgrind ${callgrind_options} $<TARGET_FILE:${target}> ${x_PROGRAM_ARGS}
+    DEPENDS ${target}
+  )
+  add_dependencies(do-all-valgrind-tests ${target_name})
+endfunction()
