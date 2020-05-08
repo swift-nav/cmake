3a4e8720b0da8bdfcb6526d353f74e76a164b0a8
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-05-08 12:54:41 -0700

    [POS-51] Update the FindStarling modules (#52)

--- FindStarling.cmake
+++ FindStarling.cmake
@@ -2,7 +2,14 @@ include("GenericFindDependency")
 option(starling_ENABLE_TESTS "" OFF)
 option(starling_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
-    TARGET starling
-    SYSTEM_HEADER_FILE "starling/starling.h"
+    TARGET pvt-runner-lib
+    SOURCE_DIR starling
+    SYSTEM_HEADER_FILE "pvt_driver/runner/pvt_runner.h"
     SYSTEM_INCLUDES
-    )
+)
+
+mark_target_as_system_includes(sensorfusion)
+mark_target_as_system_includes(pvt_driver)
+mark_target_as_system_includes(pvt-engine)
+mark_target_as_system_includes(pvt-common)
+mark_target_as_system_includes(starling-util)
