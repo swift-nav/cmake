c9225eb3275f99984c43aa1839f34464ce095af0
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2020-09-28 09:37:40 +1000

    PR changes made

--- FindNanopb.cmake
+++ FindNanopb.cmake
@@ -2,7 +2,7 @@ include("GenericFindDependency")
 option(nanopb_BUILD_GENERATOR "" OFF)
 GenericFindDependency(
   TARGET protobuf-nanopb
-  SOURCE_DIR "third_party/nanopb"
+  SOURCE_DIR "nanopb"
   SYSTEM_INCLUDES
-  )
+)
 
