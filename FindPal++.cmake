950f182e056f4d359427d4490d0fbb522ffada62
Matt Woodward <46688854+woodfell@users.noreply.github.com>
2020-09-30 20:41:44 +1000

    [ESD-1864] Deprecate libpal++ repository (#69)
    * Remove FindPal++.cmake
    
    * FindPal addition targets pal++

--- FindPal++.cmake
+++
@@ -1,6 +0,0 @@
-include("GenericFindDependency")
-option(pal++_ENABLE_TESTS "" OFF)
-GenericFindDependency(
-    TARGET pal++
-    SOURCE_DIR libpal_cpp
-)
--- FindPal.cmake
+++ FindPal.cmake
@@ -3,4 +3,5 @@ option(pal_ENABLE_TESTS "" OFF)
 option(pal_ENABLE_EXAMPLES "" OFF)
 GenericFindDependency(
   TARGET pal
+  ADDITIONAL_TARGETS pal++
 )
