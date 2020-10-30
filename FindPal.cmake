60d75d5c33321ff38b94bebe6aa8ee5786bbfcc7
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2020-10-30 16:26:27 +1100

    Disable pal test libs by default (#70)
    Co-authored-by: Matt Woodward <matthew.woodward@swift-nav.com>

--- FindPal.cmake
+++ FindPal.cmake
@@ -1,6 +1,7 @@
 include("GenericFindDependency")
 option(pal_ENABLE_TESTS "" OFF)
 option(pal_ENABLE_EXAMPLES "" OFF)
+option(pal_ENABLE_TEST_LIBS "" OFF)
 GenericFindDependency(
   TARGET pal
   ADDITIONAL_TARGETS pal++
