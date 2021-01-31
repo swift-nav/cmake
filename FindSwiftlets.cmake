0d2e894b59f8c73f832532b6225300b32a702ab5
Matt Woodward <matthew.woodward@swift-nav.com>
2021-02-01 09:50:24 +1100

    typo

--- FindSwiftlets.cmake
+++ FindSwiftlets.cmake
@@ -1,6 +1,6 @@
 include("GenericFindDependency")
 option(swiftlets_ENABLE_TESTS "" OFF)
-swiftlets_ENABLE_TEST_LIBS "" OFF)
+option(swiftlets_ENABLE_TEST_LIBS "" OFF)
 GenericFindDependency(
     TARGET swiftlets
     SOURCE_DIR swiftlets
