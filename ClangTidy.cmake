d2b069a274b0f3bea84a29dab4aa549f24a37c8a
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2021-02-28 22:32:19 +1100

    undid previous commit

--- ClangTidy.cmake
+++ ClangTidy.cmake
@@ -118,12 +118,12 @@ function(create_clang_tidy_targets)
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     add_custom_target(clang-tidy-all-check
-        COMMAND test ! -s fixes.yaml
+        COMMAND test ! -f fixes.yaml
         DEPENDS clang-tidy-all
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
     add_custom_target(clang-tidy-diff-check
-        COMMAND test ! -s fixes.yaml
+        COMMAND test ! -f fixes.yaml
         DEPENDS clang-tidy-diff
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
@@ -248,9 +248,6 @@ function(swift_setup_clang_tidy)
   # First search for an appropriate clang-tidy
   if(NOT x_CLANG_TIDY_NAMES)
     set(x_CLANG_TIDY_NAMES
-        run-clang-tidy     run-clang-tidy.py
-        run-clang-tidy-6.0 run-clang-tidy-6.0.py
-        run-clang-tidy-4.0 run-clang-tidy-4.0.py
         clang-tidy60 clang-tidy-6.0
         clang-tidy40 clang-tidy-4.0
         clang-tidy39 clang-tidy-3.9
@@ -288,11 +285,11 @@ function(swift_setup_clang_tidy)
     create_clang_tidy_targets(
         TOP_LEVEL ${top_level_project}
         ALL_COMMAND
-        ${${PROJECT_NAME}_CLANG_TIDY} ${x_EXTRA_ARGS} -p ${CMAKE_BINARY_DIR} -export-fixes=${CMAKE_CURRENT_SOURCE_DIR}/fixes.yaml
+        ${${PROJECT_NAME}_CLANG_TIDY} ${x_EXTRA_ARGS} -p ${CMAKE_BINARY_DIR} --export-fixes=${CMAKE_CURRENT_SOURCE_DIR}/fixes.yaml
           `git ls-files ${srcs}`
         DIFF_COMMAND
         git diff --diff-filter=ACMRTUXB --quiet --name-only master -- ${srcs} ||
-        ${${PROJECT_NAME}_CLANG_TIDY} ${x_EXTRA_ARGS} -p ${CMAKE_BINARY_DIR} -export-fixes=${CMAKE_CURRENT_SOURCE_DIR}/fixes.yaml
+        ${${PROJECT_NAME}_CLANG_TIDY} ${x_EXTRA_ARGS} -p ${CMAKE_BINARY_DIR} --export-fixes=${CMAKE_CURRENT_SOURCE_DIR}/fixes.yaml
           `git diff --diff-filter=ACMRTUXB --name-only master -- ${srcs}`
         )
   endif()
