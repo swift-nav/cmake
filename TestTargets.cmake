3a6f1a7225c7f225ea3c651e1597caef897deddc
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2021-04-01 12:04:12 +1100

    ESD-1981 code coverage overhaul (#79)

--- TestTargets.cmake
+++ TestTargets.cmake
@@ -9,6 +9,7 @@
 # Basic usage is
 #
 # swift_add_test(test-suite-name
+#    <UNIT_TEST|INTEGRATION_TEST>
 #    SRCS
 #      main.cc
 #      ...more source files
@@ -24,6 +25,10 @@
 #   linked against the specified libraries
 # - do-test-suite-name - A custom target which will execute the above target
 #
+# UNIT_TEST or INTEGRATION_TEST options should be specified for each function
+# call, it serves to categorize if the test executable is a unit test or an
+# integration test. If either option is not specified, a cmake warning will be
+# raised. If both are specified, a cmake error will be raised.
 #
 # LINK, INCLUDE, and WORKING_DIRECTORY are optional parameters. 
 #
@@ -46,11 +51,12 @@
 # from the googletest package. The non-parallel target is always created.
 #
 # The other function, swift_add_test_runner(), can be used to create a do-... 
-# target which pointer at some other command. This can be used to invoke a test 
+# target which points at some other command. This can be used to invoke a test
 # which is actually a shell script or some other executable which is available 
 # but not built from source
 #
 # swift_add_test_runner(test-suite-name
+#    <UNIT_TEST|INTEGRATION_TEST>
 #    COMMAND <testing command> <arguments>
 #    WORKING_DIRECTORY <path>
 #    DEPENDS <optional list of cmake targets on which this test depend>
@@ -58,6 +64,8 @@
 #
 # This will create just a single target:
 # - do-test-suite-name - Execute the given command
+#
+# UNIT_TEST and INTEGRATION_TEST options work identical to swift_add_test
 #
 # WORKING_DIRECTORY and DEPENDS are optional arguments. WORKING_DIRECTORY defaults
 # to ${CMAKE_CURRENT_BINARY_DIR}
@@ -72,7 +80,14 @@
 #
 # This module will create some extra global targets to build and run all tests
 # - build-all-tests - Build all specified tests
-# - do-all-tests - Execute all specified tests
+# - do-all-tests - Execute all specified tests (includes do-all-unit-tests and
+#   do-all-integration-tests)
+# - do-all-unit-tests - Executes all tests marked with the UNIT_TEST option
+# - do-all-integration_tests - Executes all tests marked with the
+#   INTEGRATION_TEST option
+#
+# If the PARALLEL option was specified for swift_add_test, than the unit tests
+# will be run in parallel when executing do-all-tests.
 #
 # In addition tests can be added with the option POST_BUILD which will cause 
 # cmake to execute those tests as part of the 'all' target. To assist this 
@@ -96,6 +111,8 @@
 #    COMMAND <path to executable>
 #    )
 #
+# NOTE: using POST_BUILD option is not advised as it will increase build time
+#
 # Dependency chains are set up so that post build tests will be run towards the 
 # end of the build process. Cmake lacks functionality to run commands as a 
 # post-build step so it is not guaranteed that tests will run after everything 
@@ -117,29 +134,29 @@ include(CodeCoverage)
 option(AUTORUN_TESTS "Automatically run post-build tests as part of 'all' target" ON)
 
 macro(swift_create_test_targets)
-if(AUTORUN_TESTS)
-  set(autorun ALL)
-endif()
+  if(AUTORUN_TESTS)
+    set(autorun ALL)
+  endif()
 
-if(NOT TARGET build-all-tests)
-  add_custom_target(build-all-tests)
-endif()
+  if(NOT TARGET build-all-tests)
+    add_custom_target(build-all-tests)
+  endif()
 
-if(NOT TARGET do-all-tests)
-  add_custom_target(do-all-tests)
-endif()
+  if(NOT TARGET do-all-tests)
+    add_custom_target(do-all-tests)
+  endif()
 
-if(NOT TARGET build-post-build-tests)
-  add_custom_target(build-post-build-tests ${autorun})
-endif()
+  if(NOT TARGET build-post-build-tests)
+    add_custom_target(build-post-build-tests ${autorun})
+  endif()
 
-if(NOT TARGET do-post-build-tests)
-  add_custom_target(do-post-build-tests ${autorun})
-endif()
+  if(NOT TARGET do-post-build-tests)
+    add_custom_target(do-post-build-tests ${autorun})
+  endif()
 endmacro()
 
 function(swift_add_test_runner target)
-  set(argOption "POST_BUILD")
+  set(argOption "INTEGRATION_TEST" "POST_BUILD" "UNIT_TEST")
   set(argSingle "COMMENT" "WORKING_DIRECTORY")
   set(argMulti "COMMAND" "DEPENDS")
 
@@ -159,6 +176,12 @@ function(swift_add_test_runner target)
     set(wd WORKING_DIRECTORY ${x_WORKING_DIRECTORY})
   endif()
 
+  if (NOT x_INTEGRATION_TEST AND NOT x_UNIT_TEST)
+    message(WARNING "Missing INTEGRATION_TEST or UNIT_TEST option")
+  elseif(x_INTEGRATION_TEST AND x_UNIT_TEST)
+    message(FATAL_ERROR "Both INTEGRATION_TEST and UNIT_TEST option were specified, you can only specify one")
+  endif()
+
   add_custom_target(
       do-${target}
       COMMAND ${x_COMMAND}
@@ -168,6 +191,22 @@ function(swift_add_test_runner target)
   add_dependencies(do-all-tests do-${target})
   if(x_DEPENDS)
     add_dependencies(do-${target} ${x_DEPENDS})
+  endif()
+
+  if (x_INTEGRATION_TEST)
+    if (NOT TARGET do-all-integration-tests)
+      add_custom_target(do-all-integration-tests)
+    endif()
+
+    add_dependencies(do-all-integration-tests do-${target})
+  endif()
+
+  if (x_UNIT_TEST)
+    if (NOT TARGET do-all-unit-tests)
+      add_custom_target(do-all-unit-tests)
+    endif()
+
+    add_dependencies(do-all-unit-tests do-${target})
   endif()
 
   if(x_POST_BUILD)
@@ -186,7 +225,7 @@ function(swift_add_test_runner target)
 endfunction()
 
 function(swift_add_test target)
-  set(argOption "PARALLEL" "POST_BUILD")
+  set(argOption "INTEGRATION_TEST" "PARALLEL" "POST_BUILD" "UNIT_TEST")
   set(argSingle "COMMENT" "WORKING_DIRECTORY")
   set(argMulti "SRCS" "LINK" "INCLUDE")
 
@@ -208,6 +247,12 @@ function(swift_add_test target)
 
   if(x_WORKING_DIRECTORY)
     set(wd WORKING_DIRECTORY ${x_WORKING_DIRECTORY})
+  endif()
+
+  if (NOT x_INTEGRATION_TEST AND NOT x_UNIT_TEST)
+    message(WARNING "Missing INTEGRATION_TEST or UNIT_TEST option")
+  elseif(x_INTEGRATION_TEST AND x_UNIT_TEST)
+    message(FATAL_ERROR "Both INTEGRATION_TEST and UNIT_TEST option were specified, you can only specify one")
   endif()
 
   add_executable(${target} EXCLUDE_FROM_ALL ${x_SRCS})
@@ -238,7 +283,36 @@ function(swift_add_test target)
   endif()
 
   add_dependencies(build-all-tests ${target})
-  add_dependencies(do-all-tests do-${target})
+
+  if(x_PARALLEL)
+    add_dependencies(do-all-tests parallel-${target})
+  else()
+    add_dependencies(do-all-tests do-${target})
+  endif()
+
+  if (x_INTEGRATION_TEST)
+    if (NOT TARGET do-all-integration-tests)
+      add_custom_target(do-all-integration-tests)
+    endif()
+
+    if(x_PARALLEL)
+      add_dependencies(do-all-integration-tests parallel-${target})
+    else()
+      add_dependencies(do-all-integration-tests do-${target})
+    endif()
+  endif()
+
+  if (x_UNIT_TEST)
+    if (NOT TARGET do-all-unit-tests)
+      add_custom_target(do-all-unit-tests)
+    endif()
+
+    if(x_PARALLEL)
+      add_dependencies(do-all-unit-tests parallel-${target})
+    else()
+      add_dependencies(do-all-unit-tests do-${target})
+    endif()
+  endif()
 
   if(x_POST_BUILD)
     add_custom_target(
