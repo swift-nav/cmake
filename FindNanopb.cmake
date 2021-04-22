5402a8f28bedb3235f65a23d9b58688fb4c62b18
Jason Mobarak <jason@swiftnav.com>
2021-04-22 14:06:41 -0700

    meta repo

--- FindNanopb.cmake
+++ FindNanopb.cmake
@@ -1,8 +1,8 @@
 include("GenericFindDependency")
-option(nanopb_BUILD_GENERATOR "" OFF)
+option(nanopb_BUILD_GENERATOR "" ON)
+
 GenericFindDependency(
   TARGET protobuf-nanopb
   SOURCE_DIR "nanopb"
   SYSTEM_INCLUDES
 )
-
