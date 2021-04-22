da9f4597c07f51374f78655d7f79a007914108ff
Jason Mobarak <jason@swiftnav.com>
2021-04-22 14:56:23 -0700

    meta repo support for eigin

--- FindEigen.cmake
+++ FindEigen.cmake
@@ -6,9 +6,11 @@ add_library(eigen INTERFACE)
 
 if (DEFINED THIRD_PARTY_INCLUDES_AS_SYSTEM AND NOT THIRD_PARTY_INCLUDES_AS_SYSTEM)
   target_include_directories(eigen INTERFACE
+      ${PROJECT_SOURCE_DIR}/third_party/meta/eigen/
       ${PROJECT_SOURCE_DIR}/third_party/eigen/)
 else()
   target_include_directories(eigen SYSTEM INTERFACE
+      ${PROJECT_SOURCE_DIR}/third_party/meta/eigen/
       ${PROJECT_SOURCE_DIR}/third_party/eigen/)
 endif()
 
