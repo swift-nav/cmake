9426fb76e8fe01969355bd82da88ed3f81c0c654
Matt Woodward <46688854+woodfell@users.noreply.github.com>
2021-07-01 11:18:44 +1000

    [ESD-2113] Add license notice (#91)
    * Add MIT license notice
    
    * fix shebang lines
    
    * whitespace
    
    Co-authored-by: Jason Mobarak <jason@swiftnav.com>

--- CompileOptions.cmake
+++ CompileOptions.cmake
@@ -1,4 +1,16 @@
 #
+# Copyright (C) 2021 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+#
 # Set a common set of compiler options and warning flags
 #
 # Call swift_set_compile_options() for any target to set
--- LanguageStandards.cmake
+++ LanguageStandards.cmake
@@ -1,4 +1,16 @@
 #
+# Copyright (C) 2021 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+#
 # Offers a simple function to set a number of targets to follow the company
 # wide C/C++ standard version. One can bypass some of these standards through
 # the following options:
