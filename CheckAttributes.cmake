a6963e8b43495bdc3cc09c862f7051e70db517ac
Matt Woodward <matthew.woodward@swift-nav.com>
2021-07-19 17:30:13 +1000

    wip

--- CheckAttributes.cmake
+++ CheckAttributes.cmake
@@ -1,8 +1,23 @@
-if(TARGET check-attributes)
-  return()
-endif()
+function(create_check_attributes_target)
+  if(NOT ${PROJECT_NAME} STREQUAL ${CMAKE_PROJECT_NAME})
+    # Only create for top level projects
+    return()
+  endif()
 
-add_custom_target(check-attributes ALL
-  ${CMAKE_CURRENT_SOURCE_DIR}/cmake/common/scripts/check_attributes.sh
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  )
+  set(argOption "")
+  set(argSingle "")
+  set(argMulti "EXCLUDE")
+
+  cmake_parse_arguments(x "${argOption}" "${argSingle}" "${argMulti}" ${ARGN})
+
+  set(arguments "'*.[ch]'" "'*.[ch]pp'" "'*.cc'")
+  foreach(excl ${x_EXCLUDE})
+    list(APPEND arguments ":!:${excl}")
+  endforeach()
+
+  add_custom_target(check-attributes ALL
+    ${CMAKE_CURRENT_LIST_DIR}/cmake/common/scripts/check_attributes.sh ${arguments}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+    )
+
+endfunction()
--- scripts/check_attributes.sh
+++ scripts/check_attributes.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 failed=0
-files=$(grep -l __attribute__ $(git ls-files '*.[ch]') $(git ls-files '*.[ch]pp') $(git ls-files '*.cc'))
+files=$(grep -l __attribute__ $(git ls-files $@))
 
 if [[ -n "$files" ]];
 then
-  grep -n __attribute__ $files |
+  grep -Hn __attribute__ $files |
     while read line;
     do
-      echo $(pwd)/$line
+      # Output a message similar to a compiler error so that editors/IDEs can parse it
+      location=$(echo "$line" | cut -d: -f-2)
+      code=$(echo "$line" | cut -d: -f3-)
+      echo $(pwd)/$location: error: Do not use __attribute__, prefer one of the macros from swiftnav/macros.h
+      echo "          $code"
     done
   failed=1
 fi
