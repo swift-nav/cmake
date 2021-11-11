35ca82468fc3b69613d751eb059a24cda9fffc8e
Joseph Angelo <joseph.angelo@swift-nav.com>
2021-11-11 13:50:49 -0800

    Added variable to disable RTTI in RapidCheck

--- FindRapidCheck.cmake
+++ FindRapidCheck.cmake
@@ -14,6 +14,7 @@ cmake_minimum_required(VERSION 3.2)
 include("GenericFindDependency")
 
 set(RC_ENABLE_GTEST ON CACHE BOOL "" FORCE)
+set(RC_ENABLE_RTTI OFF CACHE BOOL "" FORCE)
 
 GenericFindDependency(
   TARGET rapidcheck
