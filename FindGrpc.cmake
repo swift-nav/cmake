5444262778984d82f4fd5b5901a3132c71e4a79d
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2022-08-30 15:29:51 +1000

    compile compile command cleanup

---
+++ FindGrpc.cmake
@@ -0,0 +1,9 @@
+include("GenericFindDependency")
+
+option(ABSL_PROPAGATE_CXX_STD "Use CMake C++ standard meta features (e.g. cxx_std_11) that propagate to targets that link to Abseil" true)
+
+GenericFindDependency(
+  TARGET grpc++
+  SOURCE_DIR grpc
+  SYSTEM_INCLUDES
+)
