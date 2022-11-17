21b7c270355a914f1a547a64f397ca9e313c73a7
lucasle-sn <lucas.le@swift-nav.com>
2022-11-17 15:54:46 +1100

    Add config libuv

---
+++ FindUv.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+include("GenericFindDependency")
+
+GenericFindDependency(
+  TARGET uv
+  SOURCE_DIR libuv
+  SYSTEM_INCLUDES
+)
