9837201c948fc05a9fe900948d5572821409395c
Matt Peddie <matt.peddie@swift-nav.com>
2022-12-13 17:05:30 +1000

    Support issue suppression for runtime sanitizers

--- SanitizeTargets.cmake
+++ SanitizeTargets.cmake
@@ -6,6 +6,7 @@ option(SWIFT_SANITIZE_MEMORY "Enable memory sanitizer." OFF)
 option(SWIFT_SANITIZE_THREAD "Enable thread sanitizer." OFF)
 option(SWIFT_SANITIZE_UNDEFINED "Enable undefined behavior sanitizer." OFF)
 option(SWIFT_SANITIZE_DATAFLOW "Enable dataflow sanitizer." OFF)
+option(SWIFT_SANITIZE_DISABLE_SUPPRESSION "Disable suppression of checks." OFF)
 
 # Some of these options can't be used simultaneously.
 #
@@ -52,6 +53,33 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     # The `vptr` sanitizer won't work with `-fno-rtti`.
     set(SWIFT_SANITIZE_FLAGS  "${SWIFT_SANITIZE_FLAGS} -fsanitize=undefined -fno-sanitize=vptr")
   endif ()
+
+  # Suppression files are supported at build time only by clang, not GCC.
+  if (NOT DEFINED SWIFT_SANITIZE_SUPPRESSION_FILE)
+    if (EXISTS "${PROJECT_ROOT}/sanitizers.supp")
+      set(SWIFT_SANITIZE_SUPPRESSION_FILE "${PROJECT_ROOT}/sanitizers.supp")
+    endif()
+  endif ()
+
+  # According to the clang docs, we have to pass this file at build
+  # time; the sanitizers that support runtime suppression can only use
+  # it for third-party code that's not been built with sanitizer support
+  # (https://clang.llvm.org/docs/AddressSanitizer.html#suppressing-errors-in-recompiled-code-ignorelist).
+  # We build all our code, including third-party libraries, so we cannot
+  # simply pass a file in an environment variable
+  # (https://clang.llvm.org/docs/AddressSanitizer.html#suppressing-reports-in-external-libraries).
+  #
+  # The docs for the sanitizers don't seem to be complete or maintained,
+  # though, nor is behavior consistent between different sanitizers, so
+  # this theory could all be wrong!
+  #
+  # TODO(@peddie): I'm not sure how to make all targets that use this
+  # depend on the suppression file contents.
+  if (NOT SWIFT_SANITIZE_DISABLE_SUPPRESSION AND (DEFINED SWIFT_SANITIZE_SUPPRESSION_FILE))
+    message(STATUS "Building with sanitizer ignore list ${SWIFT_SANITIZE_SUPPRESSION_FILE}")
+    message("    Note that this file is NOT CORRECTLY TRACKED as a dependency; if you modify its contents, you will have to force recompilation some other way.")
+    set(SWIFT_SANITIZE_FLAGS "${SWIFT_SANITIZE_FLAGS} -fsanitize-ignorelist=${SWIFT_SANITIZE_SUPPRESSION_FILE}")
+  endif()
 elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 4.8)
   # See: https://gcc.gnu.org/onlinedocs/gcc/Instrumentation-Options.html
   #
@@ -75,9 +103,17 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSI
   endif ()
   if (SWIFT_SANITIZE_UNDEFINED)
     message(STATUS "Enabling undefined behavior sanitizer.")
+    # TODO(@peddie): how can we pass `-fno-sanitize=vptr` only when
+    # RTTI is disabled?  Albatross unit tests, for example, use RTTI.
+    #
     # The `vptr` sanitizer won't work with `-fno-rtti`.
     set(SWIFT_SANITIZE_FLAGS  "${SWIFT_SANITIZE_FLAGS} -fsanitize=undefined -fno-sanitize=vptr")
   endif ()
+
+  if(DEFINED SWIFT_SANITIZE_SUPPRESSION_FILE)
+    message(WARNING "You are compiling with GCC, which does not support masking of sanitizer issues via `-fsanitize-ignorelist=`.")
+    message(WARNING "If you don't want the issues in ${SWIFT_SANITIZE_SUPPRESSION_FILE} to be flagged, please build with clang instead.")
+  endif()
 else ()
   message(FATAL_ERROR "Oh noes! We don't support your compiler.")
 endif ()
@@ -88,7 +124,9 @@ if (SWIFT_SANITIZE_ADDRESS OR SWIFT_SANITIZE_MEMORY OR SWIFT_SANITIZE_THREAD OR 
   message("    https://github.com/google/sanitizers/wiki/AddressSanitizerFlags#run-time-flags")
   message("    https://github.com/google/sanitizers/wiki/SanitizerCommonFlags")
   message("    e.g.:  ASAN_OPTIONS=check_initialization_order=true:detect_stack_use_after_return=true:strict_string_checks=true:halt_on_error=false")
+  message("    You may also specify SWIFT_SANITIZE_SUPPRESSION_FILE to mask known errors.")
   set(SWIFT_SANITIZE_FLAGS  "${SWIFT_SANITIZE_FLAGS} -fsanitize-recover=all")
 endif ()
+
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} ${SWIFT_SANITIZE_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SWIFT_SANITIZE_FLAGS}")
