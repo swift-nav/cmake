8adfcc893c900fd665b4d46caf872db7a267c662
John Michael Burke <johnmichael.burke@swift-nav.com>
2023-01-10 12:19:49 -0800

    Formatting.

--- FindMKL.cmake
+++ FindMKL.cmake
@@ -29,13 +29,16 @@
 #  endif()
 
 # If already in cache, be silent
-if (MKL_INCLUDE_DIRS AND MKL_LIBRARIES AND MKL_INTERFACE_LIBRARY AND
-    MKL_SEQUENTIAL_LAYER_LIBRARY AND MKL_CORE_LIBRARY)
-  set (MKL_FIND_QUIETLY TRUE)
+if(MKL_INCLUDE_DIRS
+   AND MKL_LIBRARIES
+   AND MKL_INTERFACE_LIBRARY
+   AND MKL_SEQUENTIAL_LAYER_LIBRARY
+   AND MKL_CORE_LIBRARY)
+  set(MKL_FIND_QUIETLY TRUE)
 endif()
 
 if(NOT BUILD_SHARED_LIBS)
-  if (WIN32)
+  if(WIN32)
     set(INT_LIB "mkl_intel_ilp64.lib")
     set(SEQ_LIB "mkl_sequential.lib")
     set(THR_LIB "mkl_intel_thread.lib")
@@ -59,31 +62,33 @@ if(MSVC)
 else()
   set(INTEL_ROOT_DEFAULT "/opt/intel")
 endif()
-set(INTEL_ROOT ${INTEL_ROOT_DEFAULT} CACHE PATH "Folder contains intel libs")
-find_path(MKL_ROOT include/mkl.h PATHS $ENV{MKLROOT} ${INTEL_ROOT}/mkl
-                                   DOC "Folder contains MKL")
+set(INTEL_ROOT
+    ${INTEL_ROOT_DEFAULT}
+    CACHE PATH "Folder contains intel libs")
+find_path(
+  MKL_ROOT include/mkl.h
+  PATHS $ENV{MKLROOT} ${INTEL_ROOT}/mkl
+  DOC "Folder contains MKL")
 
-find_path(MKL_INCLUDE_DIR NAMES mkl.h HINTS ${MKL_ROOT}/include /usr/include/mkl)
+find_path(
+  MKL_INCLUDE_DIR
+  NAMES mkl.h
+  HINTS ${MKL_ROOT}/include /usr/include/mkl)
 
+find_library(
+  MKL_INTERFACE_LIBRARY
+  NAMES ${INT_LIB}
+  PATHS ${MKL_ROOT}/lib ${MKL_ROOT}/lib/intel64 ${MKL_ROOT}/lib/intel64_win ${INTEL_ROOT}/mkl/lib/intel64)
 
-find_library(MKL_INTERFACE_LIBRARY
-             NAMES ${INT_LIB}
-             PATHS ${MKL_ROOT}/lib
-                   ${MKL_ROOT}/lib/intel64
-                   ${MKL_ROOT}/lib/intel64_win
-                   ${INTEL_ROOT}/mkl/lib/intel64)
+find_library(
+  MKL_SEQUENTIAL_LAYER_LIBRARY
+  NAMES ${SEQ_LIB}
+  PATHS ${MKL_ROOT}/lib ${MKL_ROOT}/lib/intel64 ${INTEL_ROOT}/mkl/lib/intel64)
 
-find_library(MKL_SEQUENTIAL_LAYER_LIBRARY
-             NAMES ${SEQ_LIB}
-             PATHS ${MKL_ROOT}/lib
-                   ${MKL_ROOT}/lib/intel64
-                   ${INTEL_ROOT}/mkl/lib/intel64)
-
-find_library(MKL_CORE_LIBRARY
-             NAMES ${COR_LIB}
-             PATHS ${MKL_ROOT}/lib
-                   ${MKL_ROOT}/lib/intel64
-                   ${INTEL_ROOT}/mkl/lib/intel64)
+find_library(
+  MKL_CORE_LIBRARY
+  NAMES ${COR_LIB}
+  PATHS ${MKL_ROOT}/lib ${MKL_ROOT}/lib/intel64 ${INTEL_ROOT}/mkl/lib/intel64)
 
 set(MKL_INCLUDE_DIRS ${MKL_INCLUDE_DIR})
 set(MKL_LIBRARIES ${MKL_INTERFACE_LIBRARY} ${MKL_SEQUENTIAL_LAYER_LIBRARY} ${MKL_CORE_LIBRARY})
@@ -103,22 +108,21 @@ endif()
 # message("3 ${MKL_SEQUENTIAL_LAYER_LIBRARY}")
 # message("4 ${MKL_CORE_LIBRARY}")
 
-if (MKL_INCLUDE_DIR AND
-    MKL_INTERFACE_LIBRARY AND
-    MKL_SEQUENTIAL_LAYER_LIBRARY AND
-    MKL_CORE_LIBRARY)
+if(MKL_INCLUDE_DIR
+   AND MKL_INTERFACE_LIBRARY
+   AND MKL_SEQUENTIAL_LAYER_LIBRARY
+   AND MKL_CORE_LIBRARY)
 
+  if(NOT DEFINED ENV{CRAY_PRGENVPGI}
+     AND NOT DEFINED ENV{CRAY_PRGENVGNU}
+     AND NOT DEFINED ENV{CRAY_PRGENVCRAY}
+     AND NOT DEFINED ENV{CRAY_PRGENVINTEL}
+     AND NOT MSVC)
+    set(ABI "-m64")
+  endif()
 
-    if (NOT DEFINED ENV{CRAY_PRGENVPGI} AND
-        NOT DEFINED ENV{CRAY_PRGENVGNU} AND
-        NOT DEFINED ENV{CRAY_PRGENVCRAY} AND
-        NOT DEFINED ENV{CRAY_PRGENVINTEL} AND
-        NOT MSVC)
-      set(ABI "-m64")
-    endif()
-
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMKL_LP64 ${ABI}")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_LP64 ${ABI}")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DMKL_LP64 ${ABI}")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DMKL_LP64 ${ABI}")
 
 else()
   set(MKL_INCLUDE_DIRS "")
@@ -131,7 +135,14 @@ endif()
 
 # Handle the QUIETLY and REQUIRED arguments and set MKL_FOUND to TRUE if
 # all listed variables are TRUE.
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(MKL DEFAULT_MSG MKL_LIBRARIES MKL_INCLUDE_DIRS MKL_INTERFACE_LIBRARY MKL_SEQUENTIAL_LAYER_LIBRARY MKL_CORE_LIBRARY)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  MKL
+  DEFAULT_MSG
+  MKL_LIBRARIES
+  MKL_INCLUDE_DIRS
+  MKL_INTERFACE_LIBRARY
+  MKL_SEQUENTIAL_LAYER_LIBRARY
+  MKL_CORE_LIBRARY)
 
-MARK_AS_ADVANCED(MKL_INCLUDE_DIRS MKL_LIBRARIES MKL_INTERFACE_LIBRARY MKL_SEQUENTIAL_LAYER_LIBRARY MKL_CORE_LIBRARY)
+mark_as_advanced(MKL_INCLUDE_DIRS MKL_LIBRARIES MKL_INTERFACE_LIBRARY MKL_SEQUENTIAL_LAYER_LIBRARY MKL_CORE_LIBRARY)
