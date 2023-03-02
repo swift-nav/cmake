42b86ca5861b5e282cb6c7168c77b4d9497e4ad6
Matt Woodward <matthew.woodward@swift-nav.com>
2023-03-02 14:47:03 +1100

    Remove pvt-runner-lib from search libraries in starling target

--- FindStarling.cmake
+++ FindStarling.cmake
@@ -17,19 +17,17 @@ option(starling_ENABLE_TEST_LIBS "" OFF)
 option(starling_ENABLE_EXAMPLES "" OFF)
 
 GenericFindDependency(
-  TARGET pvt-runner-lib
+  TARGET pvt-engine
   ADDITIONAL_TARGETS
     math_routines
     sensorfusion
     pvt_driver
     pvt-common
     pvt-engine
-    pvt-runner
     pvt-sbp-logging
     pvt-sizes
     pvt-version
     starling-build-config
     starling-util
   SOURCE_DIR starling
-  SYSTEM_HEADER_FILE "pvt_driver/runner/pvt_runner.h"
 )
