dcee6e8e56d6080a2920b4d38686dd09afb95562
Rodrigo Reichert <rodrigo.reichert@swiftnav.com>
2023-06-01 22:51:33 +1000

    nit

--- ClangTidy.cmake
+++ ClangTidy.cmake
@@ -298,16 +298,16 @@ CheckOptions:
   - { key: readability-identifier-naming.ClassMemberSuffix,        value: _          }
   - { key: readability-identifier-naming.PrivateMemberSuffix,      value: _          }
   - { key: readability-identifier-naming.ProtectedMemberSuffix,    value: _          }
-  - { key: readability-identifier-naming.EnumCase,                 value: CamelCase }
+  - { key: readability-identifier-naming.EnumCase,                 value: CamelCase  }
   - { key: readability-identifier-naming.EnumConstantCase,         value: UPPER_CASE }
-  - { key: readability-identifier-naming.ConstexprVariableCase,    value: CamelCase }
-  - { key: readability-identifier-naming.ConstexprVariablePrefix,  value: c         }
-  - { key: readability-identifier-naming.GlobalConstantCase,       value: CamelCase }
-  - { key: readability-identifier-naming.GlobalConstantPrefix,     value: c         }
-  - { key: readability-identifier-naming.MemberConstantCase,       value: CamelCase }
-  - { key: readability-identifier-naming.MemberConstantPrefix,     value: c         }
-  - { key: readability-identifier-naming.StaticConstantCase,       value: CamelCase }
-  - { key: readability-identifier-naming.StaticConstantPrefix,     value: c         }
+  - { key: readability-identifier-naming.ConstexprVariableCase,    value: CamelCase  }
+  - { key: readability-identifier-naming.ConstexprVariablePrefix,  value: c          }
+  - { key: readability-identifier-naming.GlobalConstantCase,       value: CamelCase  }
+  - { key: readability-identifier-naming.GlobalConstantPrefix,     value: c          }
+  - { key: readability-identifier-naming.MemberConstantCase,       value: CamelCase  }
+  - { key: readability-identifier-naming.MemberConstantPrefix,     value: c          }
+  - { key: readability-identifier-naming.StaticConstantCase,       value: CamelCase  }
+  - { key: readability-identifier-naming.StaticConstantPrefix,     value: c          }
   - { key: readability-identifier-naming.TemplateParameterIgnoredRegexp, value: 'expr-type'}
 ")
     endif ()
