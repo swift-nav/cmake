f5113f7da642bbbb16d655cb5a17fd6116914fe1
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2024-01-05 09:08:40 +1100

    ASIO C++ and ASIO Grpc libraries (#170)
    # Changes
    
    Introduced the ability to include [ASIO
    C++](https://think-async.com/Asio/) as well as [ASIO
    gRPC](https://github.com/Tradias/asio-grpc) third party libraries.
    
    # Related
    
    Similar implementation with bazel
    https://github.com/swift-nav/rules_swiftnav/pull/127

---
+++ FindAsioGrpc.cmake
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2023 Swift Navigation Inc.
+# Contact: Swift Navigation <dev@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+
+include("GenericFindDependency")
+
+GenericFindDependency(
+  TARGET asio-grpc::asio-grpc-standalone-asio
+  SOURCE_DIR asio-grpc
+  SYSTEM_INCLUDES
+)
