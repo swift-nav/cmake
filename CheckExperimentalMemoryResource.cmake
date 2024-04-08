499b74ab0e8babfb75ac2ff4f5874f3b2a8d518e
Isaac Torres <isaac@isaactorz.dev>
2024-04-08 14:32:19 -0700

    fix docs

--- CheckExperimentalMemoryResource.cmake
+++ CheckExperimentalMemoryResource.cmake
@@ -1,21 +1,16 @@
 include(CheckCXXSourceCompiles)
 
-# The libc++ implementation that ships with clang-14 still
+# The libc++ implementation that ships with clang < v16 still
 # has the memory_resource header under experimental.
 #
 # This functions checks if we are using such a standard library
-# implementation and sets a definition to be used if we need to
-# include that version of the header.
+# implementation.
 #
-function(check_experimental_memory_resource result)
+function(check_experimental_memory_resource success)
   check_cxx_source_compiles("
 #include <experimental/memory_resource>
   int main() {
     return 0;
   }
-  " success)
-
-  if(success)
-    set(${result} -DSWIFTNAV_EXPERIMENTAL_MEMORY_RESOURCE PARENT_SCOPE)
-  endif()
+  " ${success})
 endfunction()
