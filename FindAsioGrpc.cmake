52c7c046a7d5666982785948005e465589b3a2e9
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2024-05-08 15:39:25 -0300

    Additional ASIO grpc stuff

--- FindAsioGrpc.cmake
+++ FindAsioGrpc.cmake
@@ -13,7 +13,8 @@
 include("GenericFindDependency")
 
 # asio-grpc has some logic to conditionally enable boost containers, this
-# should just be default by default.
+# should just be defaulted to false since the standard library offers the same
+# functionality.
 option(ASIO_GRPC_USE_BOOST_CONTAINER "(deprecated) Use Boost.Container instead of <memory_resource>" false)
 
 GenericFindDependency(
@@ -21,3 +22,29 @@ GenericFindDependency(
   SOURCE_DIR asio-grpc
   SYSTEM_INCLUDES
 )
+
+# Older libc++ versions like the one that ships with clang < v16 doesn't offer
+# the "memory_resource" standard header which is used by the library, instead
+# we conditionally select it to use "experimental/memory_resource" which is
+# available. The conditional selection is done via a customer preprocessor
+# which we've (Swift Navigation) introduced.
+include(CheckExperimentalMemoryResource)
+check_experimental_memory_resource(IS_EXPERIMENTAL_MEMORY_RESOURCE)
+if (IS_EXPERIMENTAL_MEMORY_RESOURCE)
+  target_compile_definitions(asio-grpc-standalone-asio
+    INTERFACE
+      SWIFTNAV_EXPERIMENTAL_MEMORY_RESOURCE
+  )
+endif()
+
+# asio-grpc uses language features that are removed in c++20, the libc++
+# implementation actually removed these features unless you set a special
+# macro.
+include(CheckLibcpp)
+check_libcpp(IS_LIBCPP)
+if (IS_LIBCPP)
+  target_compile_definitions(asio-grpc-standalone-asio
+    INTERFACE
+      _LIBCPP_ENABLE_CXX20_REMOVED_FEATURES
+  )
+endif()
