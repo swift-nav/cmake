8920f2a37e0cce06f64ac15c6fc4b94c4396b297
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2025-04-28 19:06:09 +1000

    Fix importing of latest protobuf version (#181)
    # Changes
    
    In order to use the latest protobuf `v5.29.0` version, we need to tweek
    some compile options to remove the installation commands from a few
    targets, otherwise you will get the following errors:
    
    ```
    -- Performing Test HAVE_CXX_FLAG_WNO_VARIADIC_MACROS
    -- Performing Test HAVE_CXX_FLAG_WNO_VARIADIC_MACROS - Success
    -- Configuring done (139.2s)
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_absl_check" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_absl_log" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_algorithm" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_base" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_bind_front" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_bits" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_btree" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_cleanup" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_cord" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_core_headers" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_debugging" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_die_if_null" that is not in any export set.
    CMake Error: install(EXPORT "protobuf-targets" ...) includes target "libprotobuf-lite" which requires target "absl_dynamic_annotations" that is not in any export set.
    ...
    ```

--- FindGrpc.cmake
+++ FindGrpc.cmake
@@ -1,6 +1,8 @@
 include("GenericFindDependency")
 
 option(ABSL_PROPAGATE_CXX_STD "Use CMake C++ standard meta features (e.g. cxx_std_11) that propagate to targets that link to Abseil" true)
+option(protobuf_INSTALL "Install protobuf binaries and files" OFF)
+option(utf8_range_ENABLE_INSTALL "Configure installation" OFF)
 
 GenericFindDependency(
   TARGET grpc++
