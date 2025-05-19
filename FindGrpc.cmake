361035b6c59305fe1602f4297bc357c8f2caa05f
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2025-05-19 10:05:56 +1000

    Update protobuf to v25.1 (#183)
    # Changes
    
    Since we are upgrading the protobuf version, the location for the root
    CMakeLists.txt has changed, hence the update.
    
    We also updated the swift targets (ex: `swift_add_library` or
    `swift_add_executable`) so that we can now set a global default C++
    version with `SWIFT_CXX_STANDARD`.
    
    Lastly tried to unify the interface between `swift_add_test` and the
    rest of the other swift targets since `swift_add_test` was developed
    separately to the rest, hence the difference in interface.
    
    These changes will allow us to set the C++ for our Swift targets
    independently from the third party libraries.

--- FindGrpc.cmake
+++ FindGrpc.cmake
@@ -1,6 +1,6 @@
 include("GenericFindDependency")
 
-option(ABSL_PROPAGATE_CXX_STD "Use CMake C++ standard meta features (e.g. cxx_std_11) that propagate to targets that link to Abseil" true)
+option(ABSL_PROPAGATE_CXX_STD "Use CMake C++ standard meta features (e.g. cxx_std_14) that propagate to targets that link to Abseil" true)
 option(protobuf_INSTALL "Install protobuf binaries and files" OFF)
 option(utf8_range_ENABLE_INSTALL "Configure installation" OFF)
 
